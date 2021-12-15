9d944bcf63676aa11d403459bada8a4a32e60479
unknown <you@example.com>
2021-12-15 14:07:32 +0530

    make it better

--- index.js
+++ index.js
@@ -30,18 +30,24 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+if (process.env.NODE_ENV === "production") {
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
-);
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-// app.get('/', (req, res) => {
-//     res.send('Server is ready');
-// });
+    app.get('*', (req, res) =>
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+    );
+
+} else {
+    app.get('/', (req, res) => {
+        res.send('Server is ready');
+    });
+
+}
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
 });
